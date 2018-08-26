0380794e851304e7dc2e5cce358f432a5e2e5d55
johnhuang87 <hoangoregon@gmail.com>
2018-08-26 15:14:30 +0000

    fuck

--- build_all.sh
+++ build_all.sh
@@ -13,13 +13,13 @@ sudo apt-get -qq install libgflags-dev libgoogle-glog-dev liblmdb-dev
 
 # build caffe
 echo "Building Caffe, MPI status: ${CAFFE_USE_MPI}"
-cd ../../caffe-action
+cd lib/caffe-action
 [[ -d build ]] || mkdir build
 cd build
 if [ "$CAFFE_USE_MPI" == "MPI_ON" ]; then
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx" -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+OpenCV_DIR=/home/phaihoang/work/temporal-segment-networks/3rd-party/opencv-2.4.13/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx" -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
 else
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+OpenCV_DIR=/home/phaihoang/work/temporal-segment-networks/3rd-party/opencv-2.4.13/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
 fi
 if make -j32 install ; then
     echo "Caffe Built."
