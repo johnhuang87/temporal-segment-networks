891eec19a8bc432db317170d7f52190968d77a6a
johnhuang87 <42136506+johnhuang87@users.noreply.github.com>
2018-08-26 11:42:20 +0700

    removed dense flow

--- build_all.sh
+++ build_all.sh
@@ -45,20 +45,6 @@ else
     exit 1
 fi
 
-# build dense_flow
-cd ../../../
-
-echo "Building Dense Flow"
-cd lib/dense_flow
-[[ -d build ]] || mkdir build
-cd build
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
-if make -j ; then
-    echo "Dense Flow built."
-else
-    echo "Failed to build Dense Flow. Please check the logs above."
-    exit 1
-fi
 
 # build caffe
 echo "Building Caffe, MPI status: ${CAFFE_USE_MPI}"
